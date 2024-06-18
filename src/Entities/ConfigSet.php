251ebaaf249aa9171a9b7bce5d9fe2a39f9d6d60
Alan Jose <alanjsyt@gmail.com>
2024-06-18 20:54:15 -0300

    <feat> Adding ConnectorEnum, and new method getPresetFor o ConfigSet.

--- src/Entities/ConfigSet.php
+++ src/Entities/ConfigSet.php
@@ -6,6 +6,9 @@
  */
 namespace Alanjoose\Dbsocket\Entities;
 
+use Alanjoose\Dbsocket\Enums\ConnectorEnum;
+use PDO;
+
 class ConfigSet
 {
    /**
@@ -35,5 +38,37 @@ class ConfigSet
    public function get(string $key, $default = null): mixed
    {
        return $this->configSet[$key] ?? $default;
+   }
+
+    /**
+     * Get a preset based on driver.
+     * @param string $driver
+     * @return array
+     */
+   public function getPresetFor(string $driver)
+   {
+       $connectorEnum = match($driver) {
+           'mysql' => ConnectorEnum::MYSQL,
+           'sqlite' => ConnectorEnum::SQLITE,
+       };
+
+       switch($connectorEnum)
+       {
+           case ConnectorEnum::MYSQL:
+               return [
+                   PDO::ATTR_AUTOCOMMIT => 0,
+                   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                   PDO::ATTR_CASE => PDO::CASE_NATURAL,
+                   PDO::ATTR_PERSISTENT => false,
+               ];
+           break;
+           case ConnectorEnum::SQLITE:
+                return [
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::ATTR_PERSISTENT => true,
+                ];
+           break;
+       }
    }
 }
